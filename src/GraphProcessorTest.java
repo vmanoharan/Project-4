50428ba1db0d1cd637d110d1276733420f91f172
YumingMa16 <37051741+YumingMa16@users.noreply.github.com>
2018-04-12 14:47:50 -0500

    Update GraphProcessorTest.java
    Modify test

--- src/GraphProcessorTest.java
+++ src/GraphProcessorTest.java
@@ -28,65 +28,107 @@ import org.junit.Test;
  *
  * @author Yuming Ma
  */
+
 public class GraphProcessorTest {
+private GraphProcessor graphProcessor;
+private String fileName;
+private Integer numOfVertices;
+@BeforeClass
+public static void setUpBeforeClass() throws Exception {
+}
 
-	private GraphADT<String> graph;
-	
-	private static List<String> vertices;
-	
-	private static int numOfVertices = 0;
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		vertices = new ArrayList<>();
-		vertices.add("at");
-		vertices.add("it");
-		vertices.add("cat");
-		vertices.add("hat");
-		vertices.add("hot");
-		vertices.add("rat");
-		vertices.add("heat");
-		vertices.add("neat");
-		vertices.add("major");
-		vertices.add("wheat");
-		vertices.add("streak");	
-		vertices.add("husband");
-		for (String vertex : vertices)
-			numOfVertices++;
-	}
+@AfterClass
+public static void tearDownAfterClass() throws Exception {
+}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		vertices = null;
-		numOfVertices = 0;
-	}
+@Before
+public void setUp() throws Exception {
+this.graphProcessor = new GraphProcessor();
+this.fileName = "word_list.txt";
+numOfVertices = graphProcessor.populateGraph(fileName);
+}
 
-	@Before
-	public void setUp() throws Exception {
-		this.graph = new Graph<>();
-	}
+@After
+public void tearDown() throws Exception {
+this.graphProcessor = null;
+}
 
-	@After
-	public void tearDown() throws Exception {
-		this.graph = null;
-	}
-    
-    @Test
-    public void test01_isAdjacent_given_nonexistent_vertices() {
-        expected = "";
-        actual = "";
-        if (!expected.equals(actual))
-            fail("expected: " + expected + " actual: " + actual);
-    }
-    @Test
-    public void test02_insert_null() {
-        
-    }
-    @Test
-    public void test03_insert_invalid() {
-        
-    }
+//this method checks isAdjacent() in WordProcessor and see whether it returns the expected value
+@Test
+public final void test01_WordProcessor_IsAdjacent() {
+assertEquals(WordProcessor.isAdjacent("body","boy"), true);
+assertEquals(WordProcessor.isAdjacent("cute","cut"), true);
+assertEquals(WordProcessor.isAdjacent("soy","boy"), true);
+assertEquals(WordProcessor.isAdjacent("sour","sad"), false);
+assertEquals(WordProcessor.isAdjacent("ginger","ginseng"), false);
+assertEquals(WordProcessor.isAdjacent("easy","yes"), false); //if order does matter
+assertEquals(WordProcessor.isAdjacent("tease","tea"), false);
+assertEquals(WordProcessor.isAdjacent("morningstar","morning"), false);
+assertEquals(WordProcessor.isAdjacent("coming","morning"), false);
+}
 
+//this test checks whether populateGraph method returns correct number of vertices within the graph
+@Test
+public final void test02_PopulatGraph_Value() {
+assertEquals(numOfVertices, new Integer(441));
+}
+
+//this test checks whether empty arraylist will be returned after passing two same words into getShortestPath method
+@Test
+public final void test03_SameWord_getShortestPath() {
+List<String> list = new ArrayList<String>();
+graphProcessor.shortestPathPrecomputation();
+for (String word: graphProcessor.getShortestPath("whiner", "whiner")) {
+list.add(word);
+}
+assertEquals(list.isEmpty(), true);
 }
 
 
+//this test checks whether getShortestPath() returns correct path after two different words are passed into it
+@Test
+public final void test04_getShortestPath() {
+List<String> list = new ArrayList<String>();
+graphProcessor.shortestPathPrecomputation();
+for (String elem: graphProcessor.getShortestPath("RAPINE", "HOMINY")) { //TODO use another example
+list.add(elem);
+}
+assertEquals(list.get(0),"RAPINE");
+assertEquals(list.get(1),"RAVINE");
+assertEquals(list.get(2),"RAVING");
+assertEquals(list.get(3),"ROVING");
+assertEquals(list.get(4),"ROPING");
+assertEquals(list.get(5),"COPING");
+assertEquals(list.get(6),"COMING");
+assertEquals(list.get(7),"HOMING");
+assertEquals(list.get(8),"HOMINY");
+}
+
+//this test checks if the getShortestDistance() returns -1 after passing same words
+@Test
+public final void test05_SameWord_getShortestDistance() {
+graphProcessor.shortestPathPrecomputation();
+assertEquals(graphProcessor.getShortestDistance("protest", "protest"), new Integer(-1));
+assertEquals(graphProcessor.getShortestDistance("tremulous", "tremulous"), new Integer(-1));
+assertEquals(graphProcessor.getShortestDistance("peace", "peace"), new Integer(-1));
+assertEquals(graphProcessor.getShortestDistance("airfoil", "airfoil"), new Integer(-1));
+}
+
+//this test checks if the getShortestDistance() returns -1 after passing different words, but no path exist
+@Test
+public final void test05_DiffWord_nopath_getShortestDistance() {
+graphProcessor.shortestPathPrecomputation();
+assertEquals(graphProcessor.getShortestDistance("protest", "airfoil"), new Integer(-1));
+assertEquals(graphProcessor.getShortestDistance("tremulous", "airfoil"), new Integer(-1));
+assertEquals(graphProcessor.getShortestDistance("cheap", "peace"), new Integer(-1));
+assertEquals(graphProcessor.getShortestDistance("cheap", "cheap"), new Integer(-1));
+}
+
+//this test checks if the getShortestDistance() returns -1 after passing different words, which path exist
+@Test
+public final void test06_DiffWord_havepath_getShortestDistance() {
+graphProcessor.shortestPathPrecomputation();
+// TODO add examples
+}
+
+}
